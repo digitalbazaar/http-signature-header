0ea88a19019a81d361b1a9e3e90995aab786c1dc
Andrew L Jones <ajones@digitalbazaar.com>
2020-12-23 08:33:00 -0500

    Check for signature param in request.params.signature.

--- bin/util.js
+++ bin/util.js
@@ -180,13 +180,13 @@ exports.verify = async function(
     headers: includeHeaders
   };
   const request = httpSigs.parseRequest(requestOptions, options);
-  const signature = new Buffer(request.params.signature, 'base64');
-  if(!signature) {
+  if(!request.params.signature) {
     throw new Error('No signature parameter found in Authorization header');
   }
   if(!request.params.keyId) {
     throw new Error('keyId is required for verification.');
   }
+  const signature = new Buffer(request.params.signature, 'base64');
   if(httpSignatureAlgorithm.hash) {
     httpSignatureAlgorithm.hash.update(canonicalizedString);
     canonicalizedString = Buffer.from(
